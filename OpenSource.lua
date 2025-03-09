83ea928e83cb6e9f2486f5dd5d47b32902130252
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-03-09 16:45:39 +0900

    Update OpenSource.lua

--- OpenSource.lua
+++ OpenSource.lua
@@ -1,5 +1,4 @@
 --wow please don't upload to youtube another video with title ''AUTO GET METAVERSE'' with ''your script'' that copy pasted everything from there. at least credit me alright 😘😘😘😘
--- Warning: Script doesn't works yet
 
 if game.PlaceId == 136005148166028 then
 if not game.IsLoaded then
@@ -14,6 +13,7 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Duration = 36000
 })
 
+local Platform = (game.UserInputService.TouchEnabled and not game.UserInputService.MouseEnabled) and "Mobile" or "PC"
 fireclickdetector(workspace.Kitchen.Fridge.HitBox.ClickDetector)
 task.wait(1)
 fireclickdetector(workspace.Kitchen.Fridge.GrantAward.ClickDetector)
@@ -32,6 +32,11 @@ task.wait(1)
 game.Players.LocalPlayer.Character["Brewzucki"]:Activate()
 task.wait(2)
 game.Players.LocalPlayer.Character.Humanoid:UnequipTools()
+if game.Players.LocalPlayer.Backpack:FindFirstChild("Brewzucki") then
+game.Players.LocalPlayer.Backpack["Brewzucki"]:Destroy()
+else
+game.Players.LocalPlayer.Character["Brewzucki"]:Destroy()
+end
 else
 game.Players.LocalPlayer.Character["Brewzucki"]:Activate()
 task.wait(2)
@@ -60,7 +65,19 @@ game.Players.LocalPlayer.Character.Humanoid.Sit = false  -- copy paste it into y
 task.wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-14,37,49)
 task.wait(2)
-game:GetService("ReplicatedStorage").Remotes.UpdateDavidQTE:FireServer("Victory")
+repeat task.wait()
+if Platform == "Mobile" then
+if game.Players.LocalPlayer.PlayerGui.DavidShrineQTE.DavidShrineQTE.Mobile.SpawnArea:FindFirstChild("TapLabel") then
+game.Players.LocalPlayer.PlayerGui.DavidShrineQTE.DavidShrineQTE.Mobile.SpawnArea.TapLabel.Size = UDim2.new(10000, 0, 10000, 0)
+game:GetService("VirtualUser"):CaptureController()
+game:GetService("VirtualUser"):ClickButton1(Vector2.new())
+end
+elseif Platform == "PC" then
+if game.Players.LocalPlayer.PlayerGui.DavidShrineQTE.DavidShrineQTE.PC.QuickTimeLabel.Visible == true then
+game:GetService("VirtualInputManager"):SendKeyEvent(true, game.Players.LocalPlayer.PlayerGui.DavidShrineQTE.DavidShrineQTE.PC.QuickTimeLabel.Text, false, game:GetService("VirtualInputManager"))
+end
+end
+until game.Players.LocalPlayer.PlayerGui.DavidShrineQTE.DavidShrineQTE.ScoreLabel.Text == "Score: 150"
 
 elseif game.PlaceId == 7234087065 then
 if not game.IsLoaded then
@@ -82,8 +99,15 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Button1 = "Oh..."
 })
 
-task.wait(1)
+repeat task.wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Buildings["wizard twoer"].ORB.CFrame
+if fireproximityprompt then
+fireproximityprompt(workspace.Buildings["wizard twoer"].ORB.ProximityPrompt)
+end
+until workspace.Buildings:FindFirstChild("wizard twoer 2")
 
+task.wait(1)
+	
 -- Teleport to trigger
 while task.wait() do game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(249, 94, -62820) end
   
@@ -109,7 +133,10 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 
 task.wait(1)
   
-while task.wait() do game:GetService("TeleportService"):Teleport(7234087065) end -- Sowwy lol i didn't added queue_on_teleport
+local PlaceID = 7234087065 -- This is serverhop to small server in brazil
+local v0={};local v1="";local v2=os.date("!*t").hour;local v3=false;local v4=pcall(function() v0=game:GetService("HttpService"):JSONDecode(readfile("SmallServerServerHop-Nexer1234.json"));end);if  not v4 then table.insert(v0,v2);writefile("SmallServerServerHop-Nexer1234.json",game:GetService("HttpService"):JSONEncode(v0));end function TPReturner() local v5;if (v1=="") then v5=game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"   .. PlaceID   .. "/servers/Public?sortOrder=Asc&limit=100" ));else v5=game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"   .. PlaceID   .. "/servers/Public?sortOrder=Asc&limit=100&cursor="   .. v1 ));end local v6="";if (v5.nextPageCursor and (v5.nextPageCursor~="null") and (v5.nextPageCursor~=nil)) then v1=v5.nextPageCursor;end local v7=0;for v8,v9 in pairs(v5.data) do local v10=true;v6=tostring(v9.id);if (tonumber(v9.maxPlayers)>tonumber(v9.playing)) then for v12,v13 in pairs(v0) do if (v7~=0) then if (v6==tostring(v13)) then v10=false;end elseif (tonumber(v2)~=tonumber(v13)) then local v14=pcall(function() delfile("SmallServerServerHop-Nexer1234.json");v0={};table.insert(v0,v2);end);end v7=v7 + 1 ;end if (v10==true) then table.insert(v0,v6);wait();pcall(function() writefile("SmallServerServerHop-Nexer1234.json",game:GetService("HttpService"):JSONEncode(v0));wait();game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID,v6,game.Players.LocalPlayer);end);wait(4);end end end end function Teleport() while wait() do pcall(function() TPReturner();if (v1~="") then TPReturner();end end);end end
+task.wait()
+while task.wait() do Teleport() end
 
 else
 game:GetService("StarterGui"):SetCore("SendNotification",{
