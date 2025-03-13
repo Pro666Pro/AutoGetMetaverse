0617b0b0f69071e7fbe091e5246a855cc43c88e4
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-03-13 16:54:04 +0900

    Update OpenSource.lua

--- OpenSource.lua
+++ OpenSource.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/MetaverseGlove.luau'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/Gloves/MetaverseGlove.luau'))()
